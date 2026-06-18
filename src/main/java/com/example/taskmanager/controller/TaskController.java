fd06b850deddefbe6678776afad55b33ac8cb0ea
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2026-06-17 19:16:17 -0500

    feat: implement TaskController and expose REST endpoints

---
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -0,0 +1,47 @@
+package com.example.taskmanager.controller;
+
+import com.example.taskmanager.dto.TaskDto;
+import com.example.taskmanager.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDto> createTask(@RequestBody TaskDto taskDto) {
+        return ResponseEntity.ok(taskService.createTask(taskDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDto>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDto> getTaskByid(@PathVariable Integer id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDto> updateTask(
+            @PathVariable Integer id,
+            @RequestBody TaskDto taskDto
+    ) {
+       return ResponseEntity.ok(taskService.updateTask(id, taskDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Integer id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+}
