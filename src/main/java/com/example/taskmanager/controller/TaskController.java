5af88ea14a58373b110b80992acdbb864fd9b336
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2026-06-26 11:41:52 -0500

    refactor: use Lombok @RequiredArgsConstructor and set HTTP 201 for task creation

--- src/main/java/com/example/taskmanager/controller/TaskController.java
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -3,6 +3,8 @@ package com.example.taskmanager.controller;
 import com.example.taskmanager.dto.TaskDto;
 import com.example.taskmanager.service.TaskService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,16 +12,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
+@RequiredArgsConstructor
 public class TaskController {
     private final TaskService taskService;
 
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
-
     @PostMapping
     public ResponseEntity<TaskDto> createTask(@Valid @RequestBody TaskDto taskDto) {
-        return ResponseEntity.ok(taskService.createTask(taskDto));
+        return new ResponseEntity<>(taskService.createTask(taskDto), HttpStatus.CREATED);
     }
 
     @GetMapping
