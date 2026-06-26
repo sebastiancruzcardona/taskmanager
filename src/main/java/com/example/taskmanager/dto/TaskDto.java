c0b16b9008bec36d7ebc7ea94501ce0d90fd39b4
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2026-06-26 11:44:11 -0500

    refactor: simplify TaskDto using Lombok annotations

--- src/main/java/com/example/taskmanager/dto/TaskDto.java
+++ src/main/java/com/example/taskmanager/dto/TaskDto.java
@@ -2,7 +2,15 @@ package com.example.taskmanager.dto;
 
 import com.example.taskmanager.enums.TaskStatusEnum;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDto {
 
     private Integer id;
@@ -14,50 +22,4 @@ public class TaskDto {
     private String description;
 
     private TaskStatusEnum status;
-
-    // Default Constructor
-    public TaskDto() {
-    }
-
-    // All parameter Constructor
-    public TaskDto(Integer id, String title, String description, TaskStatusEnum status) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.status = status;
-    }
-
-    // Getters
-    public Integer getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public TaskStatusEnum getStatus() {
-        return status;
-    }
-
-    // Setters
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setStatus(TaskStatusEnum status) {
-        this.status = status;
-    }
 }
